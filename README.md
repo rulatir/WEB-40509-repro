83734912ae08b3b479d90ca9a19cc76b3c9d39e5
Szczepan Hołyszewski <rulatir@wp.pl>
2020-12-12 04:13:09 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# WEB-40509-repro
+
+-----
+Demonstrates issue WEB-40509 in WebStorm/PHPStorm.
+
---
+++ index.js
@@ -0,0 +1,3 @@
+import HelloDependency from "WEB-40509-dependency/hello";
+const hello = new HelloDependency();
+hello.hello();
